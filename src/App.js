135eff09185ae132cd71b6e138c125b273d8e372
hathuynguyen <100432592+hathuynguyen@users.noreply.github.com>
2022-10-24 00:18:56 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ex2 from './components/Ex2-reactjs/Ex2';
+
+const App = () => {
+  return (
+    <div>
+      <Ex2></Ex2>  
+    </div>
+  );
+};
+
+export default App;
